9f237118eac0cfdc293fed999406981f6914c902
Liam_Niisan <robbie.barham@gmail.com>
2021-01-03 23:08:25 -0500

    This commit: finishes contact form html and javascript.

---
+++ JS/contact_form.js
@@ -0,0 +1,22 @@
+function validation() {
+    var name = document.getElementById("name").value
+    var email = document.getElementById("email").value
+    var message = document.getElementById("message").value
+    
+     if(name.lenth <5) {
+        alert('Please provide your name.')
+        return false;
+     }
+
+     if(message.lenth <10) {
+        alert('Please provide a message.')
+        return false;
+     }
+
+     if(email.indexOf("@") == -1 || email.length < 6) {
+         alert('Please provide a valid email address.')
+         return false;
+     }
+     alert("Form Submitted Successfully!")
+     return true;
+}
